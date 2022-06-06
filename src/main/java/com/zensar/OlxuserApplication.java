06a2cf17f2a7dd0a92d030cec06885b685586549
Madhu Dudekula <m.dudekula1@zensar.com>
2022-06-06 15:38:12 +0530

    first commit

---
+++ src/main/java/com/zensar/OlxuserApplication.java
@@ -0,0 +1,13 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxuserApplication.class, args);
+	}
+
+}
